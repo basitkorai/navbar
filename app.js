fdf9e852d6ca687750b856b987eb6f08c9bb61ff
Basit Korai <basitalikorai7@gmail.com>
2023-04-06 13:55:08 +0500

    padding issue solved

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".navlist");
+const toggle = document.querySelector(".toggle");
+console.log(toggle);
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("toggle");
   hamburger.classList.toggle("burger");
+});
+
+window.addEventListener("resize", () => {
+  let size = window.screen.width;
+  if (size < 800) {
+    nav.classList.remove("toggle");
+    hamburger.classList.remove("burger");
+  }
 });
